a093a921b65e7f13965e97fdd397eac21ab9f4a1
Alfredo Martel <alfredo.martel@outlook.com>
2016-09-19 19:57:35 +0200

    Modify source while Running

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>My First Angular 2 App</h1>
+    <h1>Mi Primera App Angular 2</h1>
     `
 })
 export class AppComponent { }
